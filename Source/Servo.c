42839825b353a16f837ba6bdc3907184e4b20865
elliaschabloz <67370331+elliaschabloz@users.noreply.github.com>
2021-11-26 14:25:16 +0100

    Update Servo.c

--- Source/Servo.c
+++ Source/Servo.c
@@ -5,16 +5,16 @@
 
 
 void angle_servo(int alpha, TIM_TypeDef * MyTimer){
-	int alphap = alpha-180;
-	if(abs((alphap)>=135)){
+	int alphap = abs(alpha-180);
+	if(alphap<=45 || alphap>=135){
 		PWM_RapportCyclique(MyTimer, 6000);
 	} else {
-		if(alphap>-135 && alphap<0){
-				int beta = -(300/9)*((4*alphap)/6 + 30) + 6000;
+		if(alphap>45 && alphap<90){
+				int beta = -(3000/45)*(2*alphap - 90) + 9000;
 				PWM_RapportCyclique(MyTimer, beta);
-		} else {
-			int beta = (300/9)*((4*alphap)/6 - 30) + 3000;
-			PWM_RapportCyclique(MyTimer, beta);
+		} else if(alphap>=90 && alphap<135){
+				int beta = (3000/45)*(2*alphap - 90) - 3000;
+				PWM_RapportCyclique(MyTimer, beta);
 		}
 	}
 }
